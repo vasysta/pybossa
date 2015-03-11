c40aae414898fbb72d0476a7dcc18b2934ba5961
alejandrodob <alejandrodob@gmail.com>
2015-03-11 18:50:00 +0100

    Add flask-plugins

--- pybossa/core.py
+++ pybossa/core.py
@@ -66,6 +66,9 @@ def create_app(run_as_server=True):
     setup_debug_toolbar(app)
     setup_jinja2_filters(app)
     setup_newsletter(app)
+    plugin_manager.init_app(app)
+    plugin_manager.setup_plugins()
+    plugin_manager.install_plugins()
     return app
 
 
